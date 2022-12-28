e34458728b937438d8c88d179cf22ce56ff28165
Rebekah <figueroarebekah@gmail.com>
2022-12-27 22:14:14 -0600

    commit tree 9e468be35a1ee86f2e9c89b325bc6ff6698b7cf1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const mainId = document.getElementById("main");
+mainId.remove(main);
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "OUR-NAME is the champion";
